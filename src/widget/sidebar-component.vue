6171214bf7177e1b8e2a286ec8aa3ac927bbd936
zhangzhangtao <18158899797@163.com>
2018-10-26 01:30:04 +0800

    dscvsd

--- src/widget/sidebar-component.vue
+++ src/widget/sidebar-component.vue
@@ -9,14 +9,12 @@
     display: flex;
     z-index: 1000;
   }
-  /*#sidebar-component:hover{*/
-    /*transition: 1s;*/
-    /*transform: translateX(-280px);*/
-  /*}*/
   .sc-tabs{
     width: 35px;
     height: 100%;
     background-color: #000;
+    display: flex;
+    flex-direction: column-reverse;
   }
   .sc-tabs>.head-portrait{
     width: 35px;
@@ -30,10 +28,7 @@
     background-color: var(--main-color);
   }
   .sc-tabs>.head-portrait:hover>.tip{
-    transform: translateX(100px);
-    transition: 1s;
-    opacity: 1;
-    /*display: block;*/
+    transition: 0.5s;
   }
   .sc-tabs>.head-portrait>img{
     height: 26px;
@@ -41,9 +36,9 @@
     border-radius: 50%;
   }
   .sc-tabs>.head-portrait>.tip{
-    /*display: none;*/
+    display: none;
     position: absolute;
-    right: 135px;
+    right: 70px;
     width: 90px;
     height: 35px;
     line-height: 35px;
@@ -54,8 +49,39 @@
     -webkit-box-shadow: 0 0 5px rgba(0,0,0,.2);
     -moz-box-shadow: 0 0 5px rgba(0,0,0,.2);
     box-shadow: 0 0 5px rgba(0,0,0,.2);
+    cursor: pointer;
   }
-  .sc-tabs>.head-portrait>.tip>i{width: 12px;height: 12px;background-color: #494949;position: absolute;right: -6px;top: 10px;transform: rotate(45deg)}
+  .sc-tabs>.head-portrait>.tip>i{
+    width: 12px;height: 12px;
+    background-color: #494949;
+    position: absolute;
+    right: -6px;
+    top: 10px;
+    transform: rotate(45deg)
+  }
+  .sc-tabs>.car-wrap>.car{
+    display: flex;flex-direction: column;align-items: center;
+    margin: 0 5px;
+    color: white;
+    height: 100px;
+    justify-content: center;
+    cursor: pointer;
+    border-top: 1px solid #444;
+    border-bottom: 1px solid #444;
+  }
+  .sc-tabs>.car-wrap>.car>.icon-cart{
+    color: var(--main-color)
+  }
+  .sc-tabs>.car-wrap:hover{
+    background-color: var(--main-color)
+  }
+  .sc-tabs>.car-wrap:hover>.car{
+    border-top: 1px solid var(--main-color);
+    border-bottom: 1px solid var(--main-color);
+  }
+  .sc-tabs>.car-wrap:hover>.car>.icon-cart{
+    color: white
+  }
   .sc-content{
     width: 280px;
     height: 100%;
@@ -65,7 +91,18 @@
 <template>
   <div id="sidebar-component">
     <div class="sc-tabs">
-      <div class="head-portrait">
+      <!--car-wrap-->
+      <div class="car-wrap">
+        <div class="car">
+          <i class="iconfont icon-cart"></i>
+          <p style="width: 12px">购物车</p>
+        </div>
+      </div>
+      <!--head-portrait-->
+      <div
+        class="head-portrait"
+        @mouseenter="headPortraitMouseenter"
+        @mouseleave="headPortraitMouseleave">
         <img src="http://101.132.46.146:8080/elfinder/files/zhangtao25/pc/test0.png" alt="">
         <div class="tip">
           <span>会员权益</span>
@@ -84,6 +121,18 @@
           ['清香','红茶'],['可口','绿茶'],['醇厚','黑茶'],['淡雅','黄茶'],
           ['极品','茶具']
         ]
+      }
+    },
+    methods:{
+      headPortraitMouseenter(){
+        // Solving the influence of display on transform
+        $(".tip").css({'display':'block'})
+        setTimeout(()=>{
+          $(".tip").css({'transform':'translateX(35px)','opacity':1})
+        },100)
+      },
+      headPortraitMouseleave(){
+        $(".tip").css({'display':'none','transform':'translateX(0px)','opacity':0})
       }
     }
   }
